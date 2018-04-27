1cab8016eb1d96d98988cfc8a59aeaa72464102e
Fanny Villarosa <vfanny@us.ibm.com>
2018-04-27 10:58:56 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen"; 
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"]; 
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped" + " " + cardOne);
+var cardTwo = cards[3]; 
+cardsInPlay.push(cardTwo); 
+console.log("User flipped" + " " + cardTwo);
+if(cardsInPlay.length === 2);
+{if(cardsInPlay[0] === cardsInPlay[1]); 
+ 	alert("You found a match!");
+else if(cardsInPlay[0] !== cardsInPlay[1]) 
+    alert("Sorry, try again.");} 
+
+
+
+
